a1bd386ef0fb9dcee9c33303f132826029051f87
Mike Graham <mgraham@technicalmedia.com>
2015-09-04 10:12:26 -0600

    users module (fetch-client) working :)

--- skel-nav-ts/views/app.js
+++ skel-nav-ts/views/app.js
@@ -8,8 +8,6 @@ define(["require", "exports"], function (require, exports) {
                 { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
                 { route: 'flickr', name: 'flickr', moduleId: './flickr', nav: true, title: 'Flickr' },
                 { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Github Users' },
-                { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Github Users' },
-                { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Github Users' },
                 { route: 'esri-map', name: 'esri-map', moduleId: './esri-map', nav: true, title: 'ESRI Map' },
                 { route: 'child-router', name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
             ]);
