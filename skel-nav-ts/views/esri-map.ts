a57be8be305065ff2b60f7d48ead31521ded9146
Mike Graham <mgraham@technicalmedia.com>
2015-11-28 06:40:47 -0700

    updated to aurelia beta release

--- skel-nav-ts/views/esri-map.ts
+++ skel-nav-ts/views/esri-map.ts
@@ -6,7 +6,7 @@ import SpatialReference = require('esri/SpatialReference');
 import * as webMercatorUtils from 'esri/geometry/webMercatorUtils';
 
 export class EsriMap {
-  map: Map;
+    map: Map;
     constructor() { }
     attached() {
         this.map = new Map("map", {
@@ -15,18 +15,18 @@ export class EsriMap {
             basemap: "topo"
         });
 
-        this.map.on("load", ()=> {
-          console.log('on load: ' + this.map);
-          var pm:SimpleMarkerSymbol = new  SimpleMarkerSymbol();
-          pm.size = 10;
-          var wgs = new SpatialReference({"wkid": 4326});
-          var p:Point = new Point(-118, 34.5, wgs);
-          var p2 = webMercatorUtils.geographicToWebMercator(p);
-          var g:Graphic = new Graphic(p2,pm);
-          console.log(g);
-          g.setGeometry(p);
-          console.log(this.map.loaded);
-          this.map.graphics.add(g);
+        this.map.on("load", () => {
+            console.log('on load: ' + this.map);
+            var pm: SimpleMarkerSymbol = new SimpleMarkerSymbol();
+            pm.size = 10;
+            var wgs = new SpatialReference({ "wkid": 4326 });
+            var p: Point = new Point(-118, 34.5, wgs);
+            var p2 = webMercatorUtils.geographicToWebMercator(p);
+            var g: Graphic = new Graphic(p2, pm);
+            console.log(g);
+            g.setGeometry(p);
+            console.log(this.map.loaded);
+            this.map.graphics.add(g);
         });
     }
 }
